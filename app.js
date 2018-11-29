88998f8eeef40e905a78f5fb1f91e48261622a68
BuildTools <unconfigured@null.spigotmc.org>
2018-11-29 20:12:29 +0000

    cosmetics

--- app.js
+++ app.js
@@ -2,27 +2,23 @@
 var express = require('express')
 var proxy = require('http-proxy-middleware')
 
-// proxy middleware options
+
 var options = {
-  target: 'http://localhost:6000', // target host
+  target: 'http://localhost:6000', // target host the root page
   changeOrigin: true, // needed for virtual hosted sites
   ws: true, // proxy websockets
-  /*pathRewrite: {
-    '^/api/old-path': '/api/new-path', // rewrite path
-    '^/api/remove/path': '/path' // remove base path
-  },*/
-  router: {
-    // when request.headers.host == 'dev.localhost:3000',
-    // override target 'http://www.example.org' to 'http://localhost:8000'
+   router: {
+    // when 'parts.guttih.com' == 'http://localhost:6300',
+    // override target 'http://localhost:6000' to 'http://localhost:6300'
 	'parts.guttih.com': 'http://localhost:6300',
 	'voff.guttih.com': 'http://localhost:6100',
   }
 }
 
 // create the proxy (without context)
-var exampleProxy = proxy(options)
+var rootProxy = proxy(options)
 
-// mount `exampleProxy` in web server
+// mount `rootProxy` in web server
 var app = express()
-app.use('/', exampleProxy)
+app.use('/', rootProxy)
 app.listen(80)
